ecebece45dc4c362dd396a355780130089a6ed0f
FLEXTRA <81846963+FlextraApe@users.noreply.github.com>
2022-11-02 07:28:47 +1000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ import RedirectOldRemoveLiquidityPathStructure from './views/RemoveLiquidity/red
 import { RedirectPathToSwapOnly, RedirectToSwap } from './views/Swap/redirects'
 import GlobalCheckClaimStatus from './views/Collectibles/components/GlobalCheckClaimStatus'
 
-// Route-based code splitting
+// Route-based code splitting 
 // Only pool is included in the main bundle because of it's the most visited page
 const Home = lazy(() => import('./views/Home'))
 const Farms = lazy(() => import('./views/Farms'))
